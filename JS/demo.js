9901f4c41f4cd06850776799f423444db55884bb
MD NABI ALAM <nabialamspring.com>
2024-03-04 16:17:09 +0530

    JS Function;

---
+++ JS/demo.js
@@ -0,0 +1,7 @@
+console.log("Welcome to JS");
+console.log(10 + 5); // Adding two numbers
+console.log(10+"10"); // Concatenating strings with a number
+console.log('Hello' + 'World'); // Concatenating two string literals
+console.log(100 + +"100"); // Concatenating two string literals
+console.log("I am " + 23); // Concatenation of a string and a number
+
---
+++ JS/demo1.js
@@ -0,0 +1,16 @@
+var flag=true;
+console.log(flag);
+console.log(1+true);
+console.log(1+true+true);
+console.log(false-true);
+
+
+var x = "nabi1";
+x==="nabi" ?console.log("welcome") :console.log("Bye");
+
+var a1="Allah";
+var a="nabi";
+var b="NABI ALAM";
+var all=`${a1}  ${a} ${b}`;
+console.log(all);
+
